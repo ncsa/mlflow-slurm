bf23d294e1175b1ef6200e2833787cd9fdfc5148
Ben Galewsky <ben@peartreestudio.net>
2022-09-23 15:54:45 -0400

    Add ability to specify GPUs per node

--- mlflow_slurm/slurm_backend.py
+++ mlflow_slurm/slurm_backend.py
@@ -136,6 +136,9 @@ class SlurmProjectBackend(AbstractBackend):
 #SBATCH --partition={{ config.partition }}
 #SBATCH --account={{ config.account }}
 #SBATCH --export=MLFLOW_TRACKING_URI,MLFLOW_S3_ENDPOINT_URL,AWS_SECRET_ACCESS_KEY,AWS_ACCESS_KEY_ID
+{% if config.gpus_per_node %}
+#SBATCH --gpus-per-node={{ config.gpus_per_node }}
+{% endif %}
 
 {{ command }}
         """
