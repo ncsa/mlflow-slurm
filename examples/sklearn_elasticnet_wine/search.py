a4b6722244f3b5b0c8b698b9d6a6c47a17c09f6f
Ben Galewsky <ben@peartreestudio.net>
2022-09-28 11:51:48 -0500

    Prepare for multiruns

--- examples/sklearn_elasticnet_wine/search.py
+++ examples/sklearn_elasticnet_wine/search.py
@@ -1,47 +1,39 @@
 import os
-import pkgutil
-
 import click
-
 import mlflow
-from mlflow.projects.backend import loader
+
+tracking_client = mlflow.tracking.MlflowClient()
 
 
-def eval(num_runs, experiment_id):
-    alpha = 0.4
-    momentum = 0.2
-
+def run_train(experiment_id, alpha, l1_ratio, backend_config="slurm_config.json"):
     with mlflow.start_run(nested=True) as child_run:
-        x = [{
-            'path': m.module_finder.path,
-            'name': m.name,
-        } for m in list(pkgutil.iter_modules())]
-        print(x)
         p = mlflow.projects.run(
             run_id=child_run.info.run_id,
             uri=os.path.dirname(os.path.realpath(__file__)),
             entry_point="train",
             parameters={
                 "alpha": str(alpha),
-                "l1_ratio": str(momentum),
+                "l1_ratio": str(l1_ratio),
             },
             experiment_id=experiment_id,
             synchronous=False,
             backend="slurm",
-            backend_config="slurm_config.json"
+            backend_config=backend_config
         )
-        succeeded = p.wait()
-        print(succeeded)
-        return succeeded
+        mlflow.log_params({"alpha": alpha, "l1_ratio": l1_ratio})
+        return p
 
 
 @click.command(help="Perform grid search over train (main entry point).")
 @click.option("--num-runs", type=click.INT, default=32, help="Maximum number of runs to evaluate.")
+@click.option("--train-backend-config", type=click.STRING, default="slurm_config.json", help="Json file for training jobs")
 def run(num_runs):
     with mlflow.start_run() as run:
         experiment_id = run.info.experiment_id
-        result = eval(num_runs, experiment_id)
-        print("Result ", result)
+        p = run_train(experiment_id, alpha=0.4, l1_ratio=0.1)
+        success = p.wait()
+        print("Result is ", success)
+
 
 
 if __name__ == "__main__":
